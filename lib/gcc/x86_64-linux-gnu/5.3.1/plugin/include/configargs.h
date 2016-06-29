cde4971538c47e7f44154a9cdbfaafaec26749d9
sayeed99 <sayeed.afridi2009@gmail.com>
2016-06-29 15:24:41 +0530

    New Linaro GCC-5.3.1 build in Google's way with intel optimization Flags

---
+++ lib/gcc/x86_64-linux-gnu/5.3.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-5.3.1/configure --target=x86_64-linux-gnu --prefix=/home/cyborg/zenfone/toolchian6/intel/x86_64-linux-gnu --with-gnu-as --with-gnu-ld --with-libs= --enable-languages=c,c++ --enable-cloog-backend=isl --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --disable-libitm --with-arch=x86-64 --with-tune=intel --with-fpmath=sse --with-multilib-list=m32,m64,mx32 --enable-initfini-array --disable-nls --with-sysroot=/home/cyborg/zenfone/androtoolx86/sysroot --with-python --with-bugurl=sayeed.afridi2009@gmail.com --disable-bootstrap --enable-plugins --enable-libgomp --enable-gnu-indirect-function --disable-libcilkrts --disable-libsanitizer --enable-gold --enable-threads --enable-graphite=yes --enable-eh-frame-hdr-for-static --enable-gold=default --program-transform-name='s&^&x86_64-linux-android-&' --disable-option-checking";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "intel" } };
